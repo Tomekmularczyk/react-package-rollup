4d93626c68dd98f4d17150ce797cd2bece7df8f6
tomekmularczyk <tomasz.mularczyk@risendot.com>
2018-04-05 09:56:19 +0200

    configure babel and react

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-export const chuj = () => console.log('chu!');
+import React from 'react';
+
+class MyComponent extends React.Component {
+  render() {
+    return <div>hello world</div>
+  }
+}
+
+export default MyComponent;
